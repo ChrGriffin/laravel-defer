56a1a4f470cd13e4fc6558be9c79ec85050e12cd
Christian Griffin <c.griffin@vehikl.com>
2018-10-16 14:56:22 -0400

    Added blade template compiler.

---
+++ src/ImageDeferCompiler.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace ChrGriffin\LaravelDefer;
+
+use Illuminate\Filesystem\Filesystem;
+use Illuminate\View\Compilers\BladeCompiler;
+
+class ImageDeferCompiler extends BladeCompiler
+{
+    /**
+     * Blade compilers.
+     *
+     * @var array
+     */
+    public $compilers = [];
+
+    /**
+     * ImageDeferCompiler constructor.
+     *
+     * @param Filesystem $files
+     * @param string $cachePath
+     * @param array $ignoredPaths
+     * @return void
+     */
+    public function __construct(Filesystem $files, $cachePath, $ignoredPaths = [])
+    {
+        parent::__construct($files, $cachePath);
+
+        $this->ignoredPaths = $ignoredPaths;
+        $this->compilers[] = 'Defer';
+    }
+
+    /**
+     * Compile a Blade template.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function compileDefer($value)
+    {
+        if ($this->ignoredPaths) {
+            $path = str_replace('\\', '/', $this->getPath());
+            foreach ($this->ignoredPaths as $ignoredPath) {
+                if (strpos($path, $ignoredPath) !== false) {
+                    return $value;
+                }
+            }
+        }
+
+        return $this->render($value);
+    }
+
+    /**
+     * Render a Blade template.
+     *
+     * @param string $value
+     * @return string
+     */
+    public function render($value)
+    {
+        return $value;
+    }
+}
--- src/LaravelDeferServiceProvider.php
+++ src/LaravelDeferServiceProvider.php
@@ -4,6 +4,7 @@ namespace ChrGriffin\LaravelDefer;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Contracts\Container\Container;
+use Illuminate\View\Engines\CompilerEngine;
 use Illuminate\Foundation\Application as LaravelApplication;
 use Laravel\Lumen\Application as LumenApplication;
 
@@ -16,15 +17,16 @@ class LaravelDeferServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-        $this->setup();
+        $this->configure();
+        $this->configureCompiler();
     }
 
     /**
-     * Set up the package configuration.
+     * Publish and configure the package.
      *
      * @return void
      */
-    protected function setup()
+    protected function configure()
     {
         $path = realpath(__DIR__ . '/../config/defer.php');
 
@@ -39,16 +41,72 @@ class LaravelDeferServiceProvider extends ServiceProvider
     }
 
     /**
+     * Configure the compiler.
+     *
+     * @return void
+     */
+    protected function configureCompiler()
+    {
+        $this->app->view->getEngineResolver()->register('blade', function () {
+            $compiler = $this->app['defer.compiler'];
+            return new CompilerEngine($compiler);
+        });
+
+        $this->app->view->addExtension('blade.php', 'blade');
+    }
+
+    /**
      * Register the service provider.
      *
      * @return void
      */
     public function register()
     {
-        $this->app->singleton('defer', function (Container $app) {
+        $this->registerCompiler();
+        $this->registerPackage();
+    }
+
+    /**
+     * Register the Blade compiler.
+     *
+     * @return void
+     */
+    protected function registerCompiler()
+    {
+        $this->app->singleton('defer.compiler', function (Container $app) {
+            $blade = $app['defer.blade'];
+            $files = $app['files'];
+            $storagePath = $app->config->get('view.compiled');
+            return new ImageDeferCompiler($blade, $files, $storagePath);
+        });
+
+        $this->app->alias('defer.compiler', ImageDeferCompiler::class);
+    }
+
+    /**
+     * Register the main package class.
+     *
+     * @return void
+     */
+    protected function registerPackage()
+    {
+        $this->app->singleton('defer', function () {
             return new LaravelDefer();
         });
 
         $this->app->alias('defer', LaravelDefer::class);
+    }
+
+    /**
+     * Services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [
+            'defer',
+            'defer.compiler'
+        ];
     }
 }

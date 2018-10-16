61e42dcfa4a709bb21bbdd2ba00c9f0e91b6eebb
Christian Griffin <c.griffin@vehikl.com>
2018-10-16 13:49:49 -0400

    Added project scaffolding.

---
+++ src/LaravelDefer.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace ChrGriffin\LaravelDefer;
+
+class LaravelDefer
+{
+    /**
+     * LaravelDefer constructor.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+}
